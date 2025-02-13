0726a683b8091cec9eb81e7f03c971971c0fc8d1
Aquiba Yudah Benarroch Bittan <a01783710@tec.mx>
2025-02-13 16:14:30 -0600

    Added addition and substraction to operation.py

--- operations.py
+++ operations.py
@@ -1,6 +1,7 @@
 a = int(input("Give me the first number:"))
 b = int(input("Give me the second number:"))
-
+console.log('Suma de a y b ' + a+b);
+console.log('Resta de a y b' + a-b);
 # Display the addition
 
 # Display the subtraction
