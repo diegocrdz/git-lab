7aefbc85a4b884356c7ac3c68635745ef5d5b738
Aquiba Yudah Benarroch Bittan <a01783710@tec.mx>
2025-02-13 21:10:13 -0600

    Correctet operations.py

--- operations.py
+++ operations.py
@@ -1,7 +1,10 @@
-a = int(input("Give me the first number:"))
-b = int(input("Give me the second number:"))
-# Display the addition
-console.log(`Addition: ${a+b}`);
-# Display the subtraction
-console.log(`Substraction: ${a-b}`);
-# Display multiplication
+        a = int(input("Give me the first number:"))
+        b = int(input("Give me the second number:")
+
+        # Display the addition
+        print(f"Addition: {a+b}")
+
+        # Display the subtraction
+        print(f"Substraction: {a-b}")
+
+        # Display multiplication
